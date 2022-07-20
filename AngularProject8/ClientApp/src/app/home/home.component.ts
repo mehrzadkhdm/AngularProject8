7e281b2ad12ac0504108c21c054d40b83638cd84
Mehrzad Khoddam <mehrzadkhdm@gmail.com>
2022-07-20 11:56:09 -0500

    Add project files.

---
+++ AngularProject8/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { IntlService } from '@progress/kendo-angular-intl';
+import { MessageService } from '@progress/kendo-angular-l10n';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+}
---
+++ AngularProject8/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AngularProject8.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
